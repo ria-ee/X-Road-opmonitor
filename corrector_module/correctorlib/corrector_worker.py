14f966f584c906119cad72f9a02b4b0c1ed21f28
Toomas Mölder <toomas.molder@ria.ee>
2018-09-15 21:54:45 +0300

    Log warnings about duplicated documents

--- corrector_module/correctorlib/corrector_worker.py
+++ corrector_module/correctorlib/corrector_worker.py
@@ -52,11 +52,9 @@ class CorrectorWorker:
                 to_remove_queue.put(current_document['_id'])
                 duplicates += 1
                 self.db_m.mark_as_corrected(current_document)
-                """
-                :logger_manager.log_warning('batch_duplicated',
-                :'_id : ObjectId(\'' + str(current_document['_id']) + '\'),
-                :messageId : ' + str(current_document['messageId']))
-                """
+                logger_manager.log_warning('batch_duplicated',
+                '_id : ObjectId(\'' + str(current_document['_id']) + '\'),
+                messageId : ' + str(current_document['messageId']))
                 continue
 
             # Check if is database duplicated
@@ -65,11 +63,9 @@ class CorrectorWorker:
                 hash_set.add(current_document_hash)
                 duplicates += 1
                 self.db_m.mark_as_corrected(current_document)
-                """
-                :logger_manager.log_warning('database_duplicated',
-                :'_id : ObjectId(\'' + str(current_document['_id']) + '\'),
-                :messageId : ' + str(current_document['messageId']))
-                """
+                logger_manager.log_warning('database_duplicated',
+                '_id : ObjectId(\'' + str(current_document['_id']) + '\'),
+                messageId : ' + str(current_document['messageId']))
                 continue
 
             # Mark hash as seen
